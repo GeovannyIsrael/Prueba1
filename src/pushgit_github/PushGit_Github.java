bce54ebb439e474ad8595f8450aa89ce1172a4ae
Geovanny Israel <Geovanny Israel@Geovanny-Israel-PC>
2016-11-01 09:49:12 -0500

    1er Commit

--- src/pushgit_github/PushGit_Github.java
+++ src/pushgit_github/PushGit_Github.java
@@ -16,6 +16,7 @@ public class PushGit_Github {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Sub'i mi primer proyecto en Github ");
     }
     
 }
